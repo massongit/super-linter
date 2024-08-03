f1ded3538f8f40dfe166e3d941f96a3fe3c13410
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2024-08-03 16:12:33 +0900

    Update javascript_good_1.js

--- test/linters/javascript_prettier/javascript_good_1.js
+++ test/linters/javascript_prettier/javascript_good_1.js
@@ -1,4 +1,4 @@
-var http = require("http");
+var http = require('http')
 var createHandler = require("github-webhook-handler");
 var handler = createHandler({ path: "/webhook", secret: process.env.SECRET });
 
